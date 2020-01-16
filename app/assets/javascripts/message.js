fe97413cb67b4363f884bf4c52369aaadb50bc23
Shuntaro11 <shun.sea.f@gmail.com>
2020-01-16 21:49:56 +0900

    auto update message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -39,6 +39,67 @@ $(function(){
       return html;
     };
   }
+
+  var buildHTML = function(message) {
+    var html_same_new = `<div class="message-list" data-message-id=${message.id}>
+                          <div class="message-list__upper">
+                            <div class="message-list__upper__name">
+                              ${message.user_name}
+                            </div>
+                            <div class="message-list__upper__date">
+                              ${message.created_at}
+                            </div>
+                          </div>
+                          <div class="message-list__text">` 
+    if (message.content && message.image) {
+      var html = html_same_new +
+          `<p class="lower-message__content">` +
+            message.content +
+          `</p>` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    } else if (message.content) {
+      var html = html_same_new +
+          `<p class="lower-message__content">` +
+            message.content +
+          `</p>` +
+        `</div>` +
+      `</div>`
+    } else if (message.image) {
+      var html = html_same_new +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    };
+    return html;
+  };
+
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chat-main__message-list').append(insertHTML);
+        $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+        $(".new_message")[0].reset();
+        $(".submit-btn").prop("disabled", false);
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -62,4 +123,8 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   });
+
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
